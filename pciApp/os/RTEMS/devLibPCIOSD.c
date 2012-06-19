1cc70eaef1839cd24a74bdf19fe7f2daa6787683
strauman <unknown>
2012-06-19 12:57:22 -0500

    - RTEMS implementation for PCI config-space access.

--- pciApp/os/RTEMS/devLibPCIOSD.c
+++ pciApp/os/RTEMS/devLibPCIOSD.c
@@ -1,6 +1,8 @@
 
 #include <stdlib.h>
 #include <epicsAssert.h>
+#include <epicsMutex.h>
+#include <errlog.h>
 
 #include <rtems/pci.h>
 #include <rtems/endian.h>
@@ -11,6 +13,31 @@
 #include "devLibPCIImpl.h"
 #include "osdPciShared.h"
 
+static epicsMutexId rtemsGuard;
+
+/* Provide a weak symbol for BSP_pci_configuration. If the BSP does not
+ * support PCI then BSP_pci_configuration resolves to the weak symbol.
+ * If the BSP does support PCI then the 'true'/strong symbol is used
+ * IF and ONLY IF the BSP enforces linking it (a strong symbol is not
+ * pulled out of a library if a weak symbol has already be found. However,
+ * if the strong symbol is linked anyways (due to some other dependency
+ * e.g., 'pci_initialize()' from the same compilation unit) then it does
+ * override the weak one.
+ */
+
+extern rtems_pci_config_t BSP_pci_configuration __attribute__((weak));
+
+static int
+rtemsDevPCIInit(void)
+{
+int rval;
+
+	rval = sharedDevPCIInit();
+
+	rtemsGuard = epicsMutexMustCreate();
+
+	return rval;
+}
 
 static
 int rtemsDevPCIConnectInterrupt(
@@ -74,15 +101,76 @@ int rtemsDevPCIDisconnectInterrupt(
   return 0;
 }
 
+static int
+rtemsDevPCIConfigAccess(const epicsPCIDevice *dev, unsigned offset, void *pArg, DevLibPCIAccMode mode)
+{
+int           rval    = S_dev_internal;
+int           st;
+
+	if ( 0 == & BSP_pci_configuration ) {
+		/* BSP has no PCI support */
+		return S_dev_badRequest;
+	}
+
+    if(epicsMutexLock(rtemsGuard)!=epicsMutexLockOK)
+        return S_dev_internal;
+
+	if ( CFG_ACC_WRITE(mode) ) {
+		switch ( CFG_ACC_WIDTH(mode) ) {
+			default:
+			case 1:
+				st = pci_write_config_byte( dev->bus, dev->device, dev->function, (unsigned char)offset, *(uint8_t*)pArg );
+			break;
+
+			case 2:
+				st = pci_write_config_word( dev->bus, dev->device, dev->function, (unsigned char)offset, *(uint16_t*)pArg );
+			break;
+			case 4:
+				st = pci_write_config_dword( dev->bus, dev->device, dev->function, (unsigned char)offset, *(uint32_t*)pArg );
+			break;
+		}
+	} else {
+		switch ( CFG_ACC_WIDTH(mode) ) {
+			default:
+			case 1:
+				st = pci_read_config_byte( dev->bus, dev->device, dev->function, (unsigned char)offset, pArg );
+			break;
+
+			case 2:
+				st = pci_read_config_word( dev->bus, dev->device, dev->function, (unsigned char)offset, pArg );
+			break;
+			case 4:
+				st = pci_read_config_dword( dev->bus, dev->device, dev->function, (unsigned char)offset, pArg );
+			break;
+		}
+	}
+
+	if ( st ) {
+		errlogPrintf("devLibPCIOSD: Unable to %s %u bytes %s configuration space: PCIBIOS error code 0x%02x\n",
+			             CFG_ACC_WRITE(mode) ? "write" : "read",
+			             CFG_ACC_WIDTH(mode),
+			             CFG_ACC_WRITE(mode) ? "to" : "from",
+			             st);
+                       
+		rval = S_dev_internal;
+	} else {
+		rval = 0;
+	}
+
+    epicsMutexUnlock(rtemsGuard);
+
+	return rval;
+}
 
 devLibPCI prtemsPCI = {
   "native",
-  sharedDevPCIInit, NULL,
+  rtemsDevPCIInit, NULL,
   sharedDevPCIFindCB,
   sharedDevPCIToLocalAddr,
   sharedDevPCIBarLen,
   rtemsDevPCIConnectInterrupt,
-  rtemsDevPCIDisconnectInterrupt
+  rtemsDevPCIDisconnectInterrupt,
+  rtemsDevPCIConfigAccess
 };
 
 #include <epicsExport.h>
