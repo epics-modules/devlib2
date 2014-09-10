af886536dd39881cd9c8055e7979487d9da735c7
Michael Davidsaver <mdavidsaver@bnl.gov>
2014-09-10 15:22:58 -0400

    common: bswapXX for RTEMS PPC
    bswap16 and bswap32 are documented as provided by epicsMMIO.h
    but aren't for RTEMS PPC.

--- common/os/RTEMS/epicsMMIO.h
+++ common/os/RTEMS/epicsMMIO.h
@@ -39,6 +39,16 @@
 #  define nat_iowrite16 be_iowrite16
 #  define nat_iowrite32 be_iowrite32
 
+#define bswap16(value) ((epicsUInt16) (  \
+        (((epicsUInt16)(value) & 0x00ff) << 8)    |       \
+        (((epicsUInt16)(value) & 0xff00) >> 8)))
+
+#define bswap32(value) (  \
+        (((epicsUInt32)(value) & 0x000000ff) << 24)   |                \
+        (((epicsUInt32)(value) & 0x0000ff00) << 8)    |                \
+        (((epicsUInt32)(value) & 0x00ff0000) >> 8)    |                \
+        (((epicsUInt32)(value) & 0xff000000) >> 24))
+
 #elif defined(i386) || defined(__i386__) || defined(__i386) || defined(__m68k__)
 
 /* X86 does not need special handling for read/write width.
