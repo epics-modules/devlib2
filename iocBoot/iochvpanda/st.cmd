231c411b48a778dd004b40357928108ff78561f4
Michael Davidsaver <mdavidsaver@gmail.com>
2016-03-21 00:57:51 -0400

    fixup hv-panda

--- iocBoot/iochvpanda/st.cmd
+++ iocBoot/iochvpanda/st.cmd
@@ -1,14 +1,14 @@
-#!../../bin/linux-x86/explore
+#!../../bin/linux-x86_64/explore
 
 
 dbLoadDatabase("../../dbd/explore.dbd",0,0)
 explore_registerRecordDeviceDriver(pdbbase) 
 
 epicsEnvSet("BASE","a:0.0")
-dbLoadRecords("hv-panda.db","DEV=$(BASE),P-TST:PANDA:")
-dbLoadRecords("hv-panda-chan.db","DEV=$(BASE),OFFSET=0,P-TST:PANDA:CH0:")
-dbLoadRecords("hv-panda-chan.db","DEV=$(BASE),OFFSET=20,P-TST:PANDA:CH1:")
-dbLoadRecords("hv-panda-chan.db","DEV=$(BASE),OFFSET=40,P-TST:PANDA:CH2:")
-dbLoadRecords("hv-panda-chan.db","DEV=$(BASE),OFFSET=60,P-TST:PANDA:CH3:")
+dbLoadRecords("hv-panda.db","DEV=$(BASE),P=TST:PANDA:")
+dbLoadRecords("hv-panda-chan.db","DEV=$(BASE),OFFSET=0,P=TST:PANDA:CH0:")
+dbLoadRecords("hv-panda-chan.db","DEV=$(BASE),OFFSET=20,P=TST:PANDA:CH1:")
+dbLoadRecords("hv-panda-chan.db","DEV=$(BASE),OFFSET=40,P=TST:PANDA:CH2:")
+dbLoadRecords("hv-panda-chan.db","DEV=$(BASE),OFFSET=60,P=TST:PANDA:CH3:")
 
 iocInit()
