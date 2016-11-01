bdeb095f62a1ac400ca57a805d8ff87429b1ac61
Michael Davidsaver <mdavidsaver@gmail.com>
2016-11-01 16:54:48 -0400

    explore: Base 3.14 compat

--- exploreApp/src/devexplore2.cpp
+++ exploreApp/src/devexplore2.cpp
@@ -9,6 +9,9 @@
 #include <memory>
 #include <sstream>
 
+#include <errno.h>
+
+#include <epicsVersion.h>
 #include <epicsStdlib.h>
 #include <devSup.h>
 #include <drvSup.h>
@@ -44,6 +47,66 @@
 #include <epicsMMIO.h>
 
 #include "devLibPCI.h"
+
+#if EPICS_REVISION<=14
+
+#define M_stdlib 4242
+#define S_stdlib_noConversion (M_stdlib | 1) /* No digits to convert */
+#define S_stdlib_extraneous   (M_stdlib | 2) /* Extraneous characters */
+#define S_stdlib_underflow    (M_stdlib | 3) /* Too small to represent */
+#define S_stdlib_overflow     (M_stdlib | 4) /* Too large to represent */
+#define S_stdlib_badBase      (M_stdlib | 5) /* Number base not supported */
+
+epicsShareFunc int
+epicsParseULong(const char *str, unsigned long *to, int base, char **units)
+{
+    int c;
+    char *endp;
+    unsigned long value;
+
+    while ((c = *str) && isspace(c))
+        ++str;
+
+    errno = 0;
+    value = strtoul(str, &endp, base);
+
+    if (endp == str)
+        return S_stdlib_noConversion;
+    if (errno == EINVAL)    /* Not universally supported */
+        return S_stdlib_badBase;
+    if (errno == ERANGE)
+        return S_stdlib_overflow;
+
+    while ((c = *endp) && isspace(c))
+        ++endp;
+    if (c && !units)
+        return S_stdlib_extraneous;
+
+    *to = value;
+    if (units)
+        *units = endp;
+    return 0;
+}
+
+static int
+epicsParseUInt32(const char *str, epicsUInt32 *to, int base, char **units)
+{
+    unsigned long value;
+    int status = epicsParseULong(str, &value, base, units);
+
+    if (status)
+        return status;
+
+#if (ULONG_MAX > 0xffffffffULL)
+    if (value > 0xffffffffUL && value <= ~0xffffffffUL)
+        return S_stdlib_overflow;
+#endif
+
+    *to = (epicsUInt32) value;
+    return 0;
+}
+#endif
+
 
 extern
 volatile void * const exploreTestBase;
