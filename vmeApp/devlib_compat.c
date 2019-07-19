epics-modules/devlib2

commit da159386b05aac2c049e9dd31622b1d6ae740b33

simplify base version tests

vmeApp/devlib_compat.c
@@ -13,6 +13,8 @@
 #include <epicsVersion.h>
 #include <devLib.h>
 
+#include "devlibversion.h"
+
 #define epicsExportSharedSymbols
 #include <shareLib.h>
 
@@ -29,7 +31,7 @@
 
 #ifdef NEED_IFACE
 
-#if EPICS_VERSION==3 && EPICS_REVISION==14 && EPICS_MODIFICATION<12
+#if EPICS_VERSION_INT<VERSION_INT(3,14,12,0)
 /*
  * Most devlib function go through an indirection table with a null
  * implimentation provided for systems which doen't impliment some
@@ -71,7 +73,7 @@ epicsShareFunc int  devInterruptInUseVME (unsigned vectorNumber)
 
 #ifdef NEED_PIMPL
 
-#if EPICS_VERSION==3 && EPICS_REVISION==14 && EPICS_MODIFICATION<12
+#if EPICS_VERSION_INT<VERSION_INT(3,14,12,0)
 devLibVirtualOS *pdevLibVirtualOS = NULL;
 #else
 devLibVME *pdevLibVirtualOS = NULL;
@@ -79,7 +81,6 @@ devLibVME *pdevLibVirtualOS = NULL;
 
 #endif /* NEED_PIMPL */
 
-#if !(EPICS_VERSION==3 && EPICS_REVISION==14 && EPICS_MODIFICATION<10)
 #include <epicsExport.h>
 
 void devReplaceVirtualOS(void)
@@ -88,4 +89,3 @@ void devReplaceVirtualOS(void)
 }
 
 epicsExportRegistrar(devReplaceVirtualOS);
-#endif

common/devlibversion.h
@@ -1,0 +1,11 @@
+#ifndef DEVLIBVERSION_H
+#define DEVLIBVERSION_H
+
+#include <epicsVersion.h>
+
+#ifndef VERSION_INT
+#  define VERSION_INT(V,R,M,P) ( ((V)<<24) | ((R)<<16) | ((M)<<8) | (P))
+#  define EPICS_VERSION_INT VERSION_INT(EPICS_VERSION, EPICS_REVISION, EPICS_MODIFICATION, EPICS_PATCH_LEVEL)
+#endif
+
+#endif // DEVLIBVERSION_H

exploreApp/src/Makefile
@@ -5,6 +5,8 @@ include $(TOP)/configure/CONFIG
 #  ADD MACRO DEFINITIONS AFTER THIS LINE
 #=============================
 
+USR_CPPFLAGS += -I$(TOP)/common
+
 # explore app doesn't build with older vxworks (eg. tornado22)
 # don't build at all until someone asks
 LIBRARY_IOC_DEFAULT = explorepci

exploreApp/src/devexplore_irq.cpp
@@ -39,13 +39,13 @@
 
 #include <epicsMMIO.h>
 
+#include "devlibversion.h"
+
 #define epicsExportSharedSymbols
 #include "devLibPCI.h"
 
-#ifdef EPICS_VERSION_INT
 #if EPICS_VERSION_INT>=VERSION_INT(3,15,0,1)
 #  define USE_COMPLETE
-#endif
 #endif
 
 #ifndef __rtems__

pciApp/Makefile
@@ -5,6 +5,8 @@ include $(TOP)/configure/CONFIG
 #  ADD MACRO DEFINITIONS AFTER THIS LINE
 #=============================
 
+USR_CPPFLAGS += -I$(TOP)/common
+
 #=============================
 # Build the IOC application
 

vmeApp/Makefile
@@ -4,6 +4,8 @@
 TOP=..
 include $(TOP)/configure/CONFIG
 
+USR_CPPFLAGS += -I$(TOP)/common
+
 #---------------------
 # Install include files
 #

commit 3862d0a939bcf8ee07b9d1c9f605b07673d253df

Fix for old base versions. Build was broken in commit da15938

vmeApp/devlib_compat.c
@@ -81,6 +81,7 @@ devLibVME *pdevLibVirtualOS = NULL;
 
 #endif /* NEED_PIMPL */
 
+#if EPICS_VERSION_INT>=VERSION_INT(3,14,10,0)
 #include <epicsExport.h>
 
 void devReplaceVirtualOS(void)
@@ -89,3 +90,4 @@ void devReplaceVirtualOS(void)
 }
 
 epicsExportRegistrar(devReplaceVirtualOS);
+#endif