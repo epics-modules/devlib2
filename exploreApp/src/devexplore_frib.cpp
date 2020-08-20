1c954a9859e7ed8f50ae3749b34203e410f75ced
Dirk Zimoch <dirk.zimoch@psi.ch>
2020-08-20 17:06:37 +0200

    backward compatibility with base R3.14

--- exploreApp/src/devexplore_frib.cpp
+++ exploreApp/src/devexplore_frib.cpp
@@ -20,7 +20,7 @@
 #include <dbScan.h>
 #include <epicsThread.h>
 #include <recGbl.h>
-#include <dbAccessDefs.h>
+#include <dbAccess.h>
 #include <waveformRecord.h>
 #include <longoutRecord.h>
 #include <mbbiRecord.h>
