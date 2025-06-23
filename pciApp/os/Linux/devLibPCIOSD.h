7cbb0a30b87d8f6f3a2765cacc577498f8eb2ddb
Jerzy Jamroz <jerzy.jamroz@gmail.com>
2025-06-23 10:52:18 +0200

    docs: License added to devLibPCIOSD.h file.

--- pciApp/os/Linux/devLibPCIOSD.h
+++ pciApp/os/Linux/devLibPCIOSD.h
@@ -1,3 +1,9 @@
+/*************************************************************************\
+* Copyright (c) 2010 Brookhaven Science Associates, as Operator of
+*     Brookhaven National Laboratory.
+* devLib2 is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution.
+\*************************************************************************/
 
 #ifndef DEVLIBPCIOSD_H_INC
 #define DEVLIBPCIOSD_H_INC
